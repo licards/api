a8e3931250b71d987b47a84bfae16a108ed722cb
Konstantins Starovoitovs <konstantins.starovoitovs@affilicon.org>
2017-04-27 15:47:31 +0200

    API configuration and status route

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,8 @@ use Dingo\Api\Routing\Router;
 $api = app(Router::class);
 
 $api->version('v1', function(Router $api) {
+    $api->get('status', function() { return "UP"; });
+
     $api->group(['prefix' => 'auth'], function(Router $api) {
         $api->post('login', "\\App\\Http\\Controllers\\AuthController@login");
         $api->post('register', "\\App\\Http\\Controllers\\AuthController@register");
@@ -17,5 +19,4 @@ $api->version('v1', function(Router $api) {
         $api->resource('categories', CategoryController::class);
         $api->resource('decks', DeckController::class);
     });
-
 });
